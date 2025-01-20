e2066245ebdeaf0430c1072835023d8b833639db
Mikhail <elkinms85@gmail.com>
2025-01-20 19:53:45 +0200

    files from tel ran

--- weather-react-main/src/utils/constants.js
+++ weather-react-main/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const base_url = `https://api.openweathermap.org/data/2.5/weather`;
 export const api_key = import.meta.env.VITE_WEATHER_API_KEY;
-export const weather_cache_time = 60 * 1000;
+export const weather_cache_time = 6 * 1000;
